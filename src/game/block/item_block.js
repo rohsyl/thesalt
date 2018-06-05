2797eae81654d4ade85e8558a44be0ee7e36fa96
Sylvain Roh <syzin12@gmail.com>
2018-06-05 13:30:03 +0200

    boosted jump not accumulable

--- src/game/block/item_block.js
+++ src/game/block/item_block.js
@@ -48,14 +48,9 @@ ItemBlock.prototype = {
 
     effect: function(player){
         if(this.spriteSrc == IMG_PIZZA){
-            // TODO : add pizza effect
-            // boost jump by 2 for 5 seconds
             player.boostJump(2, 5);
         }
         else if(this.spriteSrc == IMG_RJ45){
-            // TODO : add rj45 effect
-            // boost speed dont work well
-            // boost jump by 2 for 10 seconds
             player.hasRJ = true;
 
         }
