76a9645f40d33cc623c58f678847d7c8972a303f
Sylvain Roh <syzin12@gmail.com>
2018-06-06 08:50:41 +0200

    Improve API

--- api/function.php
+++ api/function.php
@@ -15,6 +15,37 @@ function getPdo(){
 	return $pdo;
 }
 
+function getPlayerBestScore($playerName){
+	$pdo = getPdo();
+	$sth = $pdo->prepare("SELECT MAX(score) FROM score WHERE player LIKE :player");
+	$sth->execute(array(
+		'player' => $playerName
+	));
+
+	$score = $sth->fetchColumn(0);
+	
+	return isset($score) ? $score : 0;
+}
+
+function deletePlayerScore($playerName){
+	$pdo = getPdo();
+	$sth = $pdo->prepare("DELETE FROM score WHERE player LIKE :player");
+	$sth->execute(array(
+		'player' => $playerName
+	));
+}
+
+function insertScore($score, $player, $country){
+	$pdo = getPdo();
+	$statement = $pdo->prepare('INSERT INTO score(score, player, date, country) VALUES(:score, :player, NOW(), :country)');
+	$statement->execute(array(
+		'score' => $score,
+		'player' => $player,
+		'country' => $country
+	));
+}
+
+
 function getCountry($lat, $lng){
 	$geocode=file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$lng.'&sensor=false');
 
